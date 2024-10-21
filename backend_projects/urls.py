a8d0eb2ce477dd07e099da9155a078fb0a9a3a3e
Priyanshu Agarwal <priyanshu81212@gmail.com>
2024-10-22 00:07:31 +0530

    added expense-app open-api

--- backend_projects/urls.py
+++ backend_projects/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from rest_framework.routers import DefaultRouter
-from blogs_app.views import BlogsViewSet
-from .settings import BASE_DIR
+from django.conf.urls.static import static
+from .settings import BASE_DIR, STATIC_ROOT, STATIC_URL
 import os
 
 urlpatterns = [
@@ -11,13 +10,14 @@ urlpatterns = [
     path(r"blogs/", include("blogs_app.urls")),
     path(r"weather/", include("weather_app.urls")),
     path(r"tasks/", include("todo_app.urls")),
+    path(
+        "expense-app/api-doc/",
+        TemplateView.as_view(
+            template_name=os.path.join(
+                BASE_DIR, "templates/expense-tracker-open-api.html"
+            ),
+        ),
+        name="api-doc",
+    ),
     # TODO: Add API Doc for each app
-    # path(
-    #     "api-doc/",
-    #     TemplateView.as_view(
-    #         template_name=os.path.join(BASE_DIR, "templates/open-api.html"),
-    #         extra_context={"schema_url": BASE_DIR / "docs" / "open-api.yaml"},
-    #     ),
-    #     name="api-doc",
-    # ),
-]
+] + static(STATIC_URL, document_root=STATIC_ROOT)
---
+++ expense_tracker_app/urls.py
@@ -0,0 +1,11 @@
+import os
+from django.urls import include, path
+from django.views.generic import TemplateView
+from rest_framework.routers import DefaultRouter
+
+from backend_projects.settings import BASE_DIR
+
+# router = DefaultRouter()
+# router.register(r"", BlogsViewSet, basename="blogs")
+
+urlpatterns = []
