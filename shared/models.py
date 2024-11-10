13373e1251eddfc08e006bedacbd803cdb82a316
Priyanshu Agarwal <priyanshu81212@gmail.com>
2024-11-10 19:23:50 +0530

    BP-32,BP-34,BP-33 completed

--- shared/models.py
+++ shared/models.py
@@ -8,5 +8,8 @@ class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        abstract = True
+
     def __str__(self):
         return super().__str__()
